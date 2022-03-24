0f8d477c165b70c86854c76ac0f2be692f9db979
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-03-24 18:03:30 -0300

    feat(routes): Adiciona loginValidation à rota post /login

--- app/backend/src/routes/login.route.ts
+++ app/backend/src/routes/login.route.ts
@@ -1,17 +1,22 @@
 import { NextFunction, Request, Response, Router } from 'express';
+import loginValidation from '../middlewares/login.validation';
 import UserController from '../controllers/user.controller';
 import StatusCode from '../enums/StatusCode';
 
 const loginRoute = Router();
 
-loginRoute.post('/', async (req: Request, res: Response, next: NextFunction) => {
-  try {
-    const { email, password } = req.body;
-    const result = await UserController.login(email, password);
-    return res.status(StatusCode.OK).json(result);
-  } catch (error) {
-    next(error);
-  }
-});
+loginRoute.post(
+  '/',
+  loginValidation,
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const { email, password } = req.body;
+      const result = await UserController.login(email, password);
+      return res.status(StatusCode.OK).json(result);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
 
 export default loginRoute;
