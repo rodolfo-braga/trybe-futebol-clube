27af976159a73a1117679107f4551e104447f305
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-03-24 18:02:48 -0300

    refac: Adiciona tratamento de erros do Joi ao ErrorHandler

--- app/backend/src/middlewares/error.ts
+++ app/backend/src/middlewares/error.ts
@@ -5,6 +5,12 @@ import ErrorMessage from '../enums/ErrorMessage';
 import StatusCode from '../enums/StatusCode';
 
 const errorHandler: ErrorRequestHandler = (err, _req, res, _next) => {
+  if (err.isJoi) {
+    const status = StatusCode.UNAUTHORIZED;
+    const { message } = err.details[0];
+    return res.status(status).json({ message });
+  }
+
   const status: number = getStatusCode(err.message) || StatusCode.INTERNAL_SERVER_ERROR;
   const message: string = err.message || ErrorMessage.INTERNAL_SERVER_ERROR;
 
