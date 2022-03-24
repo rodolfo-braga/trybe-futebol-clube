7d05db0469deacb9ee69731d4fd32adad19983a1
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-03-24 15:53:26 -0300

    feat: Implementa enum ErrorMessage

---
+++ app/backend/src/enums/ErrorMessage.ts
@@ -0,0 +1,7 @@
+enum ErrorMessage {
+  INVALID_INPUT = 'Incorrect email or password',
+  EMPTY_FIELDS = 'All fields must be filled',
+  INTERNAL_SERVER_ERROR = 'Internal Server Error',
+}
+
+export default ErrorMessage;
