30563dc93a12d74b28bf443c4ff948657c6e166c
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-03-24 15:57:03 -0300

    feat(services): Implementa classe UserService

---
+++ app/backend/src/services/user.service.ts
@@ -0,0 +1,28 @@
+import * as bcrypt from 'bcryptjs';
+import ErrorMessage from '../enums/ErrorMessage';
+import User from '../database/models/User';
+import { IUser } from '../interfaces/User';
+
+export default class UserService {
+  static verifyPassword(password: string, hash: string) {
+    const isPasswordValid = bcrypt.compareSync(password, hash);
+    if (!isPasswordValid) throw new Error(ErrorMessage.INVALID_INPUT);
+  }
+
+  static async getByEmailAndPassword(email: string, password: string): Promise<IUser> {
+    const user = await User.findOne(
+      { where: { email }, raw: true },
+    );
+
+    if (!user) throw new Error(ErrorMessage.INVALID_INPUT);
+
+    UserService.verifyPassword(password, user.password);
+
+    return {
+      id: user.id,
+      username: user.username,
+      role: user.role,
+      email: user.email,
+    } as IUser;
+  }
+}
