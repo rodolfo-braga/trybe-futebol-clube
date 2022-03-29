4ba54f90b92aec874321d8833978b9297b50e53a
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-03-28 21:33:48 -0300

    test(/clubs): Ao receber uma requisição do tipo GET

---
+++ app/backend/src/tests/clubs.test.ts
@@ -0,0 +1,62 @@
+import * as sinon from 'sinon';
+import * as chai from 'chai';
+import chaiHttp = require('chai-http');
+
+import { app } from '../app';
+import Club from '../database/models/Club';
+
+import { Response } from 'superagent';
+
+chai.use(chaiHttp);
+
+const { expect } = chai;
+
+const mockedClubs = [
+	{
+		id: 1,
+		clubName: "Avaí/Kindermann"
+	},
+	{
+		id: 2,
+		clubName: "Bahia"
+	}
+]
+
+const mockedClub = {
+  id: 1,
+  clubName: "Avaí/Kindermann"
+}
+
+describe('Testando a rota /clubs', () => {
+  describe('Ao receber uma requisição do tipo GET', () => {
+    let chaiHttpResponse: Response;
+
+    before(async () => {
+      sinon
+        .stub(Club, "findAll")
+        .resolves(mockedClubs as Club[]);
+
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/clubs')
+    });
+
+    after(()=>{
+      (Club.findAll as sinon.SinonStub).restore();
+    });
+
+    it('deve retornar uma lista de times', () => {
+      expect(chaiHttpResponse.body).to.be.an('array');
+    });
+
+    it('cada item da lista possui os atributos "id" e "clubName"', () => {
+      console.log();
+      
+      expect(chaiHttpResponse.body[0]).to.have.keys(['id', 'clubName']);
+    });
+
+    it('a resposta deve ter o status 200', () => {
+      expect(chaiHttpResponse).to.have.status(200);
+    });
+  });
+});
