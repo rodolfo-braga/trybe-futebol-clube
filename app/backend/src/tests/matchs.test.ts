342ead8aba7fa463f27baec5cb3544ca6439fd1f
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-03-29 19:50:22 -0300

    test(/matchs): Ao receber uma requisição do tipo GET com a query 'inProgress=false'

--- app/backend/src/tests/matchs.test.ts
+++ app/backend/src/tests/matchs.test.ts
@@ -110,4 +110,39 @@ describe('Testando a rota /matchs', () => {
       expect(chaiHttpResponse).to.have.status(200);
     });
   });
+
+  describe('Ao receber uma requisição do tipo GET com a query "inProgress=false"', () => {
+    let chaiHttpResponse: Response;
+
+    before(async () => {
+      sinon
+        .stub(Match, "findAll")
+        .resolves(mockedResponseFilteredFalse as unknown as MatchResponse[]);
+
+      chaiHttpResponse = await chai
+        .request(app)
+        .get('/matchs')
+        .query({ inProgress: false })
+    });
+
+    after(()=>{
+      (Match.findAll as sinon.SinonStub).restore();
+    });
+
+    it('deve retornar uma lista de partidas', () => {
+      expect(chaiHttpResponse.body).to.be.an('array');
+    });
+
+    it('cada partida possui os atributos "id", "homeTeam", "homeTeamGoals", "awayTeam", "awayTeamGoals", "inProgress", "homeClub" e "awayClub"', () => {      
+      expect(chaiHttpResponse.body[0]).to.have.keys(['id', 'homeTeam', 'homeTeamGoals', 'awayTeam', 'awayTeamGoals', 'inProgress', 'homeClub', 'awayClub']);
+    });
+
+    it('o atributo inProgress possui o valor "false"', () => {
+      expect(chaiHttpResponse.body[0].inProgress).to.be.false;
+    });
+
+    it('a resposta deve ter o status 200', () => {
+      expect(chaiHttpResponse).to.have.status(200);
+    });
+  });
 });
