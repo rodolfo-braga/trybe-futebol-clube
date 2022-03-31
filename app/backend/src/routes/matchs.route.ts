34b01c9b62c1273a0778054ee64edb18c7f9c2e3
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-03-31 01:02:48 -0300

    feat(routes): Implementa rota POST /matchs

--- app/backend/src/routes/matchs.route.ts
+++ app/backend/src/routes/matchs.route.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import tokenValidation from '../middlewares/token.validation';
 import MatchController from '../controllers/match.controller';
 import StatusCode from '../enums/StatusCode';
 
@@ -15,5 +16,18 @@ matchsRoute.get('/', async (req, res) => {
   }
   return res.status(StatusCode.OK).json(matches);
 });
+
+matchsRoute.post(
+  '/',
+  tokenValidation,
+  async (req, res, next) => {
+    try {
+      const newMatch = await MatchController.createMatch(req.body);
+      return res.status(StatusCode.CREATED).json(newMatch);
+    } catch (error) {
+      next(error);
+    }
+  },
+);
 
 export default matchsRoute;
