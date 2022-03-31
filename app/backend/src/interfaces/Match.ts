416a5f1b246c836e032c0ad5d0556efd4d5c73ad
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-03-31 00:52:02 -0300

    feat: Implementa intefaces NewMatch e INewMatch

--- app/backend/src/interfaces/Match.ts
+++ app/backend/src/interfaces/Match.ts
@@ -4,3 +4,15 @@ export interface MatchResponse extends Match {
   homeClub: { clubName: string };
   awayClub: { clubName: string };
 }
+
+export interface NewMatch {
+  homeTeam: number;
+  homeTeamGoals: number;
+  awayTeam: number;
+  awayTeamGoals: number;
+  inProgress: boolean;
+}
+
+export interface INewMatch extends NewMatch {
+  id: number;
+}
