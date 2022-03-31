0a45e399c16fc33d685a74d1cfe8c307e4fc2b44
rodolfo-braga <rodolfo.braga@yahoo.com.br>
2022-03-31 00:48:46 -0300

    refac: Acrescenta mensagens de erro ao ErrorMessage

--- app/backend/src/enums/ErrorMessage.ts
+++ app/backend/src/enums/ErrorMessage.ts
@@ -2,7 +2,10 @@ enum ErrorMessage {
   INVALID_INPUT = 'Incorrect email or password',
   EMPTY_FIELDS = 'All fields must be filled',
   INTERNAL_SERVER_ERROR = 'Internal Server Error',
-  CLUB_NOT_FOUND = 'Club not found',
+  TOKEN_NOT_FOUND = 'Token not found',
+  INVALID_TOKEN = 'Expired or invalid token',
+  EQUAL_TEAMS = 'It is not possible to create a match with two equal teams',
+  TEAM_NOT_FOUND = 'There is no team with such id!',
 }
 
 export default ErrorMessage;
